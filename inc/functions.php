3616360e128db37fbe37d96a3719e59ec7be6714
Kenia Valladarez <keniavalladarez@gmail.com>
2017-04-05 23:21:10 -0400

    sort items in each catalog category page and ignores the and and a '

--- inc/functions.php
+++ inc/functions.php
@@ -9,14 +9,14 @@ function get_item_html($id,$item) {
 }
 
 function array_category($catalog, $category) {
-if ($category == null) {
-  return array_keys($catalog);
-}
   $output = array();
 
   foreach ($catalog as $id => $item) {
-    if (strtolower($category) == strtolower($item["category"])) {
+    if ($category == null OR strtolower($category) == strtolower($item["category"])) {
       $sort = $item["title"];
+      $sort = ltrim($sort, "The ");
+      $sort = ltrim($sort, "A ");
+      $sort = ltrim($sort, "An ");
       $output[$id] = $sort;
     }
   }
